27900dfb544ae8e4ffa2edf9cc5df9f4aa224fd2
Den Polusmyak <den.polusmyak@gmail.com>
2024-04-09 22:27:50 +0300

    chore: set up prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,14 @@ import { PropsWithChildren } from 'react';
 import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'Basic Auth',
-  description: 'Test application',
+	title: 'Basic Auth',
+	description: 'Test application',
 };
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/app/**/*.{ts,tsx,mdx}', './src/ui/**/*.{ts,tsx,mdx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/app/**/*.{ts,tsx,mdx}', './src/ui/**/*.{ts,tsx,mdx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
 
 export default config;
