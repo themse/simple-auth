c2d4635264a2c392064473606d1d8baabbd9cfb0
Den Polusmyak <den.polusmyak@gmail.com>
2024-04-10 23:24:47 +0300

    style: adjust the input shadow style to be lighter

--- src/ui/components/atoms/Input/Input.tsx
+++ src/ui/components/atoms/Input/Input.tsx
@@ -2,7 +2,7 @@ import { InputHTMLAttributes, forwardRef } from 'react';
 import { tv } from 'tailwind-variants';
 
 const inputVariants = tv({
-	base: 'w-full rounded-md border-none bg-gray-300/50 p-4 text-sm font-medium shadow-md shadow-black/30 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-700/80 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+	base: 'w-full rounded-md border-none bg-gray-300/50 p-4 text-sm font-medium shadow-sm shadow-black/20 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-700/80 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 });
 
 type Props = InputHTMLAttributes<HTMLInputElement>;
