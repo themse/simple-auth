d25fd0a487ba6f5c61bbc42a56c663fd92a3e3e9
Den Polusmyak <den.polusmyak@gmail.com>
2024-04-10 23:12:15 +0300

    feat: impl markup of sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,38 @@
+import 'server-only';
+
 import { Container } from '@/ui/components/templates/Container';
+import { TemplateBodyCenter } from '@/ui/components/templates/BodyCenter';
+import { Heading } from '@/ui/components/atoms/Heading';
+import { Input } from '@/ui/components/atoms/Input';
+import { Label } from '@/ui/components/atoms/Label';
+import { InputGroup } from '@/ui/components/molecules/InputGroup';
+import { Button } from '@/ui/components/atoms/Button';
 
 export default function SignIn() {
-	return <Container>SignIn</Container>;
+	return (
+		<Container>
+			<TemplateBodyCenter
+				header={
+					<Heading variant="heading-1" as="h1" weight="medium" className="py-5 text-center">
+						Login
+					</Heading>
+				}
+				contentStyles="gap-y-28 max-w-80"
+			>
+				<div className="flex flex-col gap-y-8">
+					<InputGroup>
+						<Label>Email</Label>
+						<Input placeholder="Type in your email" />
+					</InputGroup>
+					<InputGroup>
+						<Label>Password</Label>
+						<Input placeholder="Type in your password" />
+					</InputGroup>
+				</div>
+				<Button size="lg" className="mx-auto w-full max-w-56 justify-center">
+					Login
+				</Button>
+			</TemplateBodyCenter>
+		</Container>
+	);
 }
